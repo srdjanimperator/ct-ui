93f89071fbed048a920b86ab9fd7de1108804af5
Srđan Jovanović <Srdjan.Jovanovic@comtrade.com>
2021-10-30 15:00:21 +0200

    example wire up

--- projects/ct-app/src/app/app.module.ts
+++ projects/ct-app/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CtFormModule } from 'ct-form';
+import { CtGridModule } from 'ct-grid';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CtGridModule,
+    CtFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
